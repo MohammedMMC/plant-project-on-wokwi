e7d1c12e6db52bd583287aa37fa1ef1fdc2065be
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-08-27 18:52:37 +0300

    finished

---
+++ README.md
@@ -0,0 +1,13 @@
+# Smart Plant Care System
+
+The **Smart Plant Care System** is an automated solution designed to help you take better care of your plants. It monitors soil moisture, temperature, and humidity, and alerts you when your plant needs watering through a buzzer that only sounds when you are nearby. The system also features customizable RGB lighting, which you can control using a Rotary Encoder. An LCD screen displays real-time environmental data and serves as the interface for adjusting the light settings.
+
+i didn't find a soil moisture on wokwi.com so i replaced it with a potentiometer. You can remove the potentiometer and put your soil moisture sensor.
+
+## Features
+
+- **Soil Moisture Monitoring:** Continuously monitors soil moisture levels to determine if your plant needs watering.
+- **Proximity-Based Alerts:** A buzzer sounds only when motion is detected nearby, alerting you that your plant needs water.
+- **Environmental Monitoring:** Displays real-time temperature and humidity levels on an LCD screen.
+- **Customizable RGB Lighting:** Control the brightness of red, green, and blue lights individually using a Rotary Encoder.
+- **User-Friendly Interface:** An easy-to-use interface on the screen, allowing you to select colors and adjust lighting directly from the device.
--- sketch.ino
+++ sketch.ino
@@ -16,12 +16,18 @@
 #define ENCODER_DT  3
 #define ENCODER_BTN 9
 
+#define SOIL_SENSOR 20
+#define MOTION_SENSOR 22
+#define BUZZER_PIN 13
+
+int minimum_water = 400;
+
 DHT dht(DHT22_PIN, DHT22);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 int led_r = 0, led_g = 0, led_b = 0;
 
-char selectedColor = 'R'; // R, G and B
+char selectedColor = 'G'; // R, G and B
 bool isColorSelected = false;
 
 void setup() {
@@ -40,11 +46,17 @@ void setup() {
   pinMode(RGB_G_PIN, OUTPUT);
   pinMode(RGB_B_PIN, OUTPUT);
 
+  pinMode(SOIL_SENSOR, INPUT);
+  pinMode(MOTION_SENSOR, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
   pinMode(ENCODER_CLK, INPUT);
   pinMode(ENCODER_DT, INPUT);
   pinMode(ENCODER_BTN, INPUT_PULLUP);
 
+  digitalWrite(BUZZER_PIN, LOW);
   attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), readEncoder, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_BTN), buttonEncoder, FALLING);
 }
 
 
@@ -52,32 +64,51 @@ void loop() {
   // put your main code here, to run repeatedly:
   displayScreen();
   rgblight();
-
-  if (digitalRead(ENCODER_BTN) == LOW) {
-    isColorSelected = !isColorSelected;
+  if (minimum_water >= analogRead(SOIL_SENSOR) && digitalRead(MOTION_SENSOR)) {
+    digitalWrite(BUZZER_PIN, HIGH);
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
   }
+
   delay(10); // this speeds up the simulation
 }
 
+void buttonEncoder() {
+  if (digitalRead(ENCODER_BTN) == LOW) {
+    isColorSelected = !isColorSelected;
+  }
+}
+
 void readEncoder() {
-  if (isColorSelected) return;
   int dtValue = digitalRead(ENCODER_DT);
   if (dtValue == HIGH) {
     if(selectedColor == 'R') {
-      selectedColor = 'G';
+      if (isColorSelected) {
+        if (led_r < 255) led_r += 5;
+      } else {selectedColor = 'G';}
     } else if (selectedColor == 'G') {
-      selectedColor = 'B';
+      if (isColorSelected) {
+        if (led_g < 255) led_g += 5;
+      } else {selectedColor = 'B';}
     } else if (selectedColor == 'B') {
-      selectedColor = 'R';
+      if (isColorSelected) {
+        if (led_b < 255) led_b += 5;
+      } else {selectedColor = 'R';}
     }
   }
   if (dtValue == LOW) {
     if(selectedColor == 'R') {
-      selectedColor = 'B';
+      if (isColorSelected) {
+        if (led_r > 0) led_r -= 5;
+      } else {selectedColor = 'B';}
     } else if (selectedColor == 'G') {
-      selectedColor = 'R';
+      if (isColorSelected) {
+        if (led_g > 0) led_g -= 5;
+      } else {selectedColor = 'R';}
     } else if (selectedColor == 'B') {
-      selectedColor = 'G';
+      if (isColorSelected) {
+        if (led_b > 0) led_b -= 5;
+      } else {selectedColor = 'G';}
     }
   }
 }
